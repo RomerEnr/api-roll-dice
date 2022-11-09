c47c160935349304378a78274c5f922918233f3d
Eduardo Pérez <eduperz38@gmail.com>
2022-11-09 12:15:29 +0000

    Exercise 2 and server.js. >New form to write words per line. > Created dependencies for linting and typescript and express. > New app from express in service.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import { countWords } from "./modules/countWords.js";
 import { readWords } from "./modules/readWords.js";
-import { wordsInNewFile } from "./modules/wordsInNewFile.js";
+import { writeWords } from "./modules/writeWords.js";
 
 const _filePath = "src/ficheros/fichero.txt";
-export const _filePath2 = "src/ficheros/fichero2.txt";
+export const _filePath2 = "src/ficheros/fichero3.txt";
 
-const main = async () => {
-  const fullWords = await readWords(_filePath);
+const main = async() => {
+  const words = await readWords(_filePath);
   const wordsLength = await countWords(_filePath);
-  wordsInNewFile(fullWords);
-  console.log(fullWords, wordsLength);
+  const formatWords = words.join("\n");
+  writeWords(formatWords, _filePath2);
+  console.log(words, wordsLength);
 };
 
 await main();
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Dise API.");
+});
+
+app.listen(port, () => {
+  console.log(`Dise app listening on port ${port}`);
+});
