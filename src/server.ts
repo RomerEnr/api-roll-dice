4be0734ea4b7bdfb388b92563c8e149343f234cd
“Romer” <romeralvareziv@gmail.com>
2022-11-10 20:48:06 +0000

    Post feature done
    >Post route implemented
    >JSON middleware added in the server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { routeDise } from "./routes/dise.route";
-const app = express();
+import { createFilm } from "./routes/film/post.route";
+export const app = express();
 const port = 3000 || process.env.PORT;
+app.use(express.json());
 
 app.get("/", (req: any, res: any) => {
   res.send("Dise API.");
 });
 
+
 // Use: http://localhost:3000/Dise?sides=[number]
 routeDise(app);
+createFilm(app);
 
 app.listen(port, () => {
   console.log(`Dise app listening on port ${port}`);
